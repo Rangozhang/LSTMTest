8c4babd47ef4656755bc15eb89d70fb247879cca
Rangozhang <rangozhang@sina.com>
2016-04-03 02:01:56 -0400

    make data longer

--- data/test/syntheticDataGenerator.py
+++ data/test/syntheticDataGenerator.py
@@ -5,19 +5,17 @@ import math
 
 n_class = 10
 n_data = 10000
-f = open('gt.txt','w')
+f = open('gt.txt','r')
 f2 = open('input.txt', 'w')
-for i in xrange(1,n_class+1):
-    length = random.randint(4, 6)
-    seq = random.sample(string.lowercase, length)
-    print str(i) + ' ' + ''.join(seq)
-    f.write(str(i)+' '+''.join(seq)+'\n')
+for line in f:
+    tokens = line.strip().split()
+    seq = list(tokens[1])
+    print(seq)
     for j in xrange(n_data):
         data_line = ''.join(random.sample(string.uppercase, random.randint(0,3)))
 
         for each in seq:
             data_line += each + ''.join(random.sample(string.uppercase, random.randint(0,3)))
         data_line += ''.join(random.sample(string.uppercase, random.randint(3,4)))
-        f2.write(str(i)+' '+data_line+'\n')
+        f2.write(tokens[0]+' '+data_line+'\n')
 
-#os.system('python syntheticTestDataGenerator.py')
